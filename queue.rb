ed60a5a9a76620b25aed62b2842ca58e9cecdc53
Rebecca Kleinberg <rebeccakleinberg@gmail.com>
2016-08-28 09:57:08 -0400

    set up queue class, plan out methods

--- queue.rb
+++ queue.rb
@@ -1,9 +1,20 @@
-class myQueue
-	
-	attr_reader
+# my own work - life at whole foods
+# first in, first out
+# T is the data type
+# queue will have a first node and a last node, which you can quickly access, but not in between
+# add: if last node is not null (i.e. defined), last.next = T object which has item as its data, new last is the T object  
+# remove: returns the data (not the node, not the queue) - and removes the first node in the queue
+# peek: returns the data but does remove node from the queue
+# strech: write a recursive function that will remove all nodes in the queue ** pops all elements off ** 
 
-	def initialize
-		@container = []
+class MyQueue
+	
+	attr_reader :data, :next
+
+	def initialize(data)
+		@data = data
+		@first = nil
+		@last = nil
 	end
 
 	def add(item)
@@ -18,3 +29,12 @@ class myQueue
 	def isEmpty?
 	end
 end
+
+q = MyQueue.new
+q.add("1")
+q.add("2")
+q.add("3") # q.container = ["1", "2", "3"]
+p q.container.to_a
+puts "****"
+q.remove
+p q.container
